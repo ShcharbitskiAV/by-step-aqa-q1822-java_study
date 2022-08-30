ebfc3fd4b3e93599ad32ce21c8f902b93edd62e3
ShcharbitskiAV <scarbicki@gmail.com>
2022-08-30 23:11:02 +0300

    add new files

---
+++ src/by/step/teach_java/class_work/cw_21072022/Task2.java
@@ -0,0 +1,18 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int[] array = new int[12];
+        int lastIndex = 0;
+        int minNumber = -15;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 31) - 15;
+
+            if (minNumber <= array[i]) {
+                minNumber = array[i];
+                lastIndex = i;
+            }
+        }
+        System.out.println("Last index in array: " + lastIndex);
+
+    }
+}
---
+++ src/by/step/teach_java/class_work/cw_21072022/Task4.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] firstArray = new int[5];
+        int[] secondArray = new int[5];
+        for (int i = 0; i < firstArray.length; i++) {
+            firstArray[i] = (int) (Math.random() * 5);
+        }
+
+        for (int i = 0; i < secondArray.length; i++) {
+            firstArray[i] = (int) (Math.random() * 5);
+        }
+
+        System.out.print(Arrays.toString(firstArray));
+        System.out.println();
+        System.out.print(Arrays.toString(secondArray));
+
+        double medium = 0;
+        double medium1 = 0;
+
+        for (int i = 0; i < 5; i++) {
+            medium = medium + firstArray[i];
+            medium1 = medium1 + secondArray[i];
+
+            medium = medium / 5;
+            medium1 = medium1 / 5;
+        }
+
+        System.out.println();
+
+        if (medium > medium1) {
+            System.out.println("Array #1 more array #2");
+        } else {
+            System.out.println("Array #2 more array #1");
+        }
+
+        if (medium == 0 && medium1 == 0) {
+            System.out.println("Arrays are equal");
+        }
+
+    }
+}
