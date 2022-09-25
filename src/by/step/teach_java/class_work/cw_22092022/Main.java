a543ae7db8d9b8e48cc27f898867a3c9d734b4bf
ShcharbitskiAV <scarbicki@gmail.com>
2022-09-26 01:54:47 +0300

    update code in files

--- src/by/step/teach_java/class_work/cw_22092022/Main.java
+++ src/by/step/teach_java/class_work/cw_22092022/Main.java
@@ -1,2 +1,24 @@
-package by.step.teach_java.class_work.cw_22092022;public class Main {
+package by.step.teach_java.class_work.cw_22092022;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Country strana = new Country();
+        strana.setName("Belarus");
+        strana.setContinent("Europe");
+        strana.toString();
+
+        City gorod = new City();
+        gorod.setName("Vitebsk");
+        gorod.setCountry("Belarus");
+        gorod.setCountOfPeople(370000);
+        gorod.toString();
+
+        City gorod2 = new City();
+        gorod2.setName("Wroclaw");
+        gorod2.setCountry("Spain");
+        gorod2.setCountOfPeople(1250000);
+        gorod2.toString();
+
+    }
 }
